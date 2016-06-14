3a3628f2cc0a1e025f8af7a8285e106aa13568e8
Nabil Sourour <bill.sourour@arcnovus.com>
2016-06-14 07:51:50 -0400

    refactor

--- src/twitter-face.d.ts
+++
@@ -1,3 +0,0 @@
-export interface TwitterFace {
-    messageNewFollower(msg?: string): void
-}
--- src/twitter-face.ts
+++ src/twitter-face.ts
@@ -2,51 +2,71 @@
 import env = require('dotenv');
 import Twit = require('twit');
 import moment = require('moment');
+import { Readable } from 'stream';
 
-// grab all my secrets
+let T: Twit;
+let stream: Twit.Twitter.Stream;
 env.config();
 
 export = {
-    messageNewFollower
+    followUser,
+    sendMessage,
+    startMessagingNewFollowers,
+    stopMessagingNewFollowers
 }
 
-function messageNewFollower(msg?: string): void {
-
-    const T = new Twit({
-        consumer_key: process.env.T_CONSUMER_KEY,
-        consumer_secret: process.env.T_CONSUMER_SECRET,
-        access_token: process.env.T_ACCESS_TOKEN,
-        access_token_secret: process.env.T_ACCESS_TOKEN_SECRET,
-        timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests. 
-    });
-
-    // setup a user stream, as per Twitter's Streaming API (https://dev.twitter.com/streaming/overview)
-    let stream = T.stream('user');
+function startMessagingNewFollowers(msg?: string): void {
 
     // when someone new follows me
-    stream.on('follow', onFollow);
+    getUserStream().on('follow', onFollow);
 
     function onFollow(data: any): void {
         let follower: Twit.Twitter.User = data.source;
-        followBack(T, follower);
-        sendMessage(T, follower, msg)
+        followUser(follower);
+        sendMessage(follower, msg)
     }
 }
 
-function followBack(T: Twit, follower: any): void {
-    T.post('friendships/create', { screen_name: follower.screen_name });
+function followUser(user: Twit.Twitter.User): void {
+    getT().post('friendships/create', { screen_name: user.screen_name });
 }
 
-function sendMessage(T: Twit, follower: any, msg?: string) {
-    let dayOfWeek: string = getDayOfWeek(follower.utc_offset);
-    msg = msg || `Hi ${follower.name}, thanks for following me. I hope you're having a great ${dayOfWeek}!`;
-    T.post('direct_messages/new', { screen_name: follower.screen_name, text: msg });
+function sendMessage(user: Twit.Twitter.User, msg?: string) {
+    let dayOfWeek: string = getDayOfWeek(user.utc_offset);
+    msg = msg || `Hi ${user.name}, thanks for following me. I hope you're having a great ${dayOfWeek}!`;
+    getT().post('direct_messages/new', { screen_name: user.screen_name, text: msg });
+}
+
+function stopMessagingNewFollowers(): void {
+    getUserStream().stop();
 }
 
 function getDayOfWeek(utcOffset: number): string {
     let weekdays: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     // get the day that it is for the user following me (0=Sunday, 6=Saturday)
-    let dayIndex: number = moment().utcOffset(utcOffset).day();
+    let dayIndex: number = moment(new Date()).utcOffset(utcOffset).day();
+    console.log(utcOffset)
     // get the name of the day of the week
     return weekdays[dayIndex];
+}
+
+function getT(): Twit {
+    if (!T) {
+        console.log("T set");
+        T = new Twit({
+            consumer_key: process.env.T_CONSUMER_KEY,
+            consumer_secret: process.env.T_CONSUMER_SECRET,
+            access_token: process.env.T_ACCESS_TOKEN,
+            access_token_secret: process.env.T_ACCESS_TOKEN_SECRET,
+            timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests. 
+        });
+    }
+    return T;
+}
+
+function getUserStream(): Twit.Twitter.Stream {
+    if(!stream) {
+        stream = <Twit.Twitter.Stream>getT().stream("user")
+    }
+    return stream;
 }
