ba3347afc8afb2ac45cbba0c3e9d63af464584fc
Nabil Sourour <bill.sourour@arcnovus.com>
2016-06-14 09:50:14 -0400

    Added geoJson and ES6 Promise for Twit

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="globals/dotenv/index.d.ts" />
+/// <reference path="globals/geojson/index.d.ts" />
 /// <reference path="globals/moment-node/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/es6-promise/index.d.ts" />
 /// <reference path="modules/twit/index.d.ts" />
