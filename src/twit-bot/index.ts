5ab43cec09d170730f275012ed6143d8e51bb060
Nabil Sourour <bill.sourour@arcnovus.com>
2016-07-10 22:06:19 -0400

    fixed utc offset bug

--- src/twit-bot/index.ts
+++ src/twit-bot/index.ts
@@ -2,13 +2,13 @@
 'use strict';
 import env = require('dotenv');
 import Twit = require('twit');
-import moment = require('moment-timezone');
+import moment = require('moment');
 
 export = {
-    followUser,
-    sendMessage,
-    startMessagingNewFollowers,
-    stopMessagingNewFollowers
+    followUser, 
+    sendMessage, 
+    startMessagingNewFollowers, 
+    stopMessagingNewFollowers 
 }
 
 /////////////////////////////
@@ -21,8 +21,8 @@ env.config();
 
 /**
  * When a user follows you, follow them back and send a message
- *
- * @param {string} [msg] - the message to send.
+ * 
+ * @param {string} [msg] - the message to send. 
  * Defaults to the environment variable: `process.env.T_GREETING`.
  * Can also use merge tags in the form, `%FIELD_NAME%` where `FIELD_NAME` is one of:
  * `DAY_OF_WEEK`, `FIRST_NAME`, or the upper case form of any Twitter User field
@@ -44,7 +44,7 @@ function startMessagingNewFollowers(msg?: string): void {
 
 /**
  * Follows a user.
- *
+ * 
  * @param {Twit.Twitter.User} user - the user to follow.
  */
 function followUser(user: Twit.Twitter.User): void {
@@ -53,9 +53,9 @@ function followUser(user: Twit.Twitter.User): void {
 
 /**
  * Sends a direct message (DM) to a user.
- *
+ * 
  * @param {Twit.Twitter.User} user - the user to message
- * @param {string} [msg] - the message to send.
+ * @param {string} [msg] - the message to send. 
  * Defaults to the environment variable: `process.env.T_GREETING`.
  * Can also use merge tags in the form, `%FIELD_NAME%`
  */
@@ -79,32 +79,33 @@ function stopMessagingNewFollowers(): void {
  * `DAY_OF_WEEK`, `FIRST_NAME`, or the upper case form of any Twitter User field
  * found at: https://dev.twitter.com/overview/api/users
  * @private
- *
+ * 
  * @param {Twit.Twitter.User} user - a Twitter user whose details you'd like to include in a message.
  * @param {string} msg - the message into which you wish to merge user details.
  */
 function mergeMessage(user: Twit.Twitter.User, msg: string): string {
     let mergedMessage = msg;
     if (msg.indexOf("%DAY_OF_WEEK%") > -1) {
-        let dayOfWeek: string = getDayOfWeek(user.time_zone);
+        let offset: number = adjustOffset(user.utc_offset),
+            dayOfWeek: string = getDayOfWeek(offset);
         mergedMessage = mergedMessage.replace("%DAY_OF_WEEK%", dayOfWeek);
     }
-
+    
     if(msg.indexOf("%FIRST_NAME%") > -1) {
         let firstName = user.name.split(' ')[0];
         mergedMessage = mergedMessage.replace("%FIRST_NAME%", firstName);
     }
-
+    
     // map merge tags to user fields.
     Object.keys(user).forEach(v => mergedMessage = mergedMessage.replace(`%${v.toUpperCase()}%`, user[v]));
-
+    
     return mergedMessage;
 }
 
 /**
  * Gets a user stream from the Twitter Streaming API
- * @private
- *
+ * @private 
+ * 
  * @returns {Twit.Stream} - a user stream
  */
 function getUserStream(): Twit.Stream {
@@ -117,7 +118,7 @@ function getUserStream(): Twit.Stream {
 /**
  * Gets an instance of Twit
  * @private
- *
+ * 
  * @returns {Twit} - an instance of Twit
  */
 function getT(): Twit {
@@ -128,23 +129,46 @@ function getT(): Twit {
             consumer_secret: process.env.T_CONSUMER_SECRET,
             access_token: process.env.T_ACCESS_TOKEN,
             access_token_secret: process.env.T_ACCESS_TOKEN_SECRET,
-            timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+            timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests. 
         });
     }
     return T;
 }
 
 /**
+ * Helper function for working with momentjs that 
+ * converts a utc offset from seconds to minutes and avoids hours.
+ * @private
+ * 
+ * @param {number} utcOffsetInSeconds - the utc offset in seconds, as provided by Twitter
+ * @returns {number} - the UTC offset in minutes. 
+ */
+function adjustOffset(utcOffsetInSeconds: number): number {
+    // convert utc offset from seconds to minutes (don't divide 0!)
+    let offset = (utcOffsetInSeconds !== 0) ? utcOffsetInSeconds / 60 : utcOffsetInSeconds;
+    console.log(offset);
+
+    // momentjs interprets values between -16 and 16 as hours instead of minutes. So we adjust accordingly. 
+    // I don't think this will ever happen because offsets are generally not expressed 
+    // to this degree of precision. But, just in case, this *hack* is precise enough for our purposes (for now).
+    if (offset >= -16 && offset <= 16) {
+        offset = 0;
+    }
+    return offset;
+}
+
+/**
  * Gets the current day of the week for a given UTC offset
  * @private
- *
+ * 
  * @param {number} utcOffset - the offset to apply to the date
  * @returns {string} - full name of the current weekday for the given UTC offset
  */
-function getDayOfWeek(timezone: string): string {
+function getDayOfWeek(utcOffsetInMinutes: number): string {
     let weekdays: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     // get the day that it is for the user following me (0=Sunday, 6=Saturday)
-    let dayIndex: number = moment().tz(timezone).day();
+    let dayIndex: number = moment().utc().utcOffset(utcOffsetInMinutes).day();
+    console.log(utcOffsetInMinutes);
     // get the name of the day of the week
     return weekdays[dayIndex];
 }
