0ef504409568582ec81b3302d58e64dd7bf4e23e
Nabil Sourour <bill.sourour@arcnovus.com>
2016-07-10 21:08:00 -0400

    switch to moment-timezone

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="globals/dotenv/index.d.ts" />
 /// <reference path="globals/geojson/index.d.ts" />
 /// <reference path="globals/moment-node/index.d.ts" />
+/// <reference path="globals/moment-timezone/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/es6-promise/index.d.ts" />
