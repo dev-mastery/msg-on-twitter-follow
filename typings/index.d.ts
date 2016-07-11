2811f72059e03ba813807a91b5a7572e9f800425
Nabil Sourour <bill.sourour@arcnovus.com>
2016-07-10 22:16:30 -0400

    remove moment-timezone

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="globals/dotenv/index.d.ts" />
 /// <reference path="globals/geojson/index.d.ts" />
 /// <reference path="globals/moment-node/index.d.ts" />
-/// <reference path="globals/moment-timezone/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/es6-promise/index.d.ts" />
